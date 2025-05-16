99c6a8348ffbebb57998485f52cb0b76d16881f5
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-05-16 20:56:18 +0330

    feat: add loading state while fetching places and pass it to Places component to render fallback text dynamically

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,15 +3,18 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   // Wrap fetch in useEffect to run it only once after the initial render,
   // avoiding repeated fetches and potential infinite loops caused by state updates
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places); // Update state with the fetched places
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -21,6 +24,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
