e2bcdb502cfe536ca8017045cb5ca0566e6b2af9
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-05-17 21:50:12 +0330

    add fetchUserPlaces function to http.js file in order to fetch user places from backend

--- src/http.js
+++ src/http.js
@@ -11,6 +11,19 @@ export async function fetchAvailablePlaces() {
   return resData.places;
 }
 
+export async function fetchUserPlaces() {
+  // Attempt to fetch data from the backend API
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+
+  // Manually throw an error if the response status is not OK (e.g., 404 or 500)
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places.");
+  }
+
+  return resData.places;
+}
+
 export async function updateUserPlaces(places) {
   // Send a PUT request to the backend API to update the user's selected places
   const response = await fetch("http://localhost:3000/user-places", {
